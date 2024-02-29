f14b069cd34cfec76935b6327e96fad0f73ae142
Sanige <103402828+Sanige@users.noreply.github.com>
2024-02-29 13:44:52 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Kanda International</title>
+<link href='http://fonts.googleapis.com/css?family=Cabin:400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#kanda-nav">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	      	</button>
+	     	<a class="navbar-brand" href="/">
+	     		<img src="images/logo-img.png" alt="Kanda International Insurance Brokers">
+	     	</a>
+	    </div>
+		<div class="collapse navbar-collapse" id="kanda-nav">
+			<ul class="nav navbar-nav navbar-right" id="menu">
+				<li data-menuanchor="home"><a href="#home"><span>Home</span></a></li>
+				<li data-menuanchor="whykanda"><a href="#whykanda"><span>Why Kanda</span></a></li>
+				<li data-menuanchor="aboutus"><a href="#aboutus"><span>About Us</span></a></li>
+				<li data-menuanchor="services"><a href="#services"><span>Services</span></a></li>
+				<li data-menuanchor="ourstaff"><a href="#ourstaff"><span>Our Staff</span></a></li>
+				<li data-menuanchor="contactus"><a href="#contactus"><span>Contact Us</span></a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div id="loading" class="loading-site">
+	<div class="spinner"></div>
+</div>
+<div id="kanda">
+<section class="section home-section" id="home">
+	<div class="container" id="intro">
+		<div class="row">
+			<div class="col-md-12">
+				<h1><small>Welcome to </small> Kanda International</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p>We offer a wide range of Insurance, Products and Services to assist our valuable customers locally and worldwide. Kanda International Limited commenced operations in early January 2003 as Kanda International Insurance Brokers and Risk Consultants.</p>
+			</div>
+			<div class="col-md-6">
+				<p>Kanda is licensed as a General Insurance Broker by the Government under the Insurance Act 1995. We are dynamic customer oriented organisation that is drive by a passion for delivering service with integrity to our customers.</p>
+			</div>
+		</div>
+	</div>
+	<div id="kanda-home" class="carousel kanda-home carousel-fade" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="item active" style="background-image: url(images/home-bg.jpg);"></div>
+			<div class="item" style="background-image: url(images/7.jpg);"></div>
+		</div>
+	</div>
+</section>
+<section class="section why-section" id="why">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h2>Why Kanda International</h2>
+			</div>
+		</div>
+		<div class="clearfix why-box">
+			<div class="row">
+				<div class="box-w col-md-4">
+					<div class="box">
+						We are insurance brokers and we work for our clients NOT for the insurance companies. Hence, you get the best cover that you need.
+					</div>
+				</div>
+				<div class="box-w col-md-8">
+					<div class="box">
+						We are able to place cover on your behalf with a wide range of insurance companies and underwriting agencies locally and/or globally. You get a choice, without the need to run around Insurance Companies.
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="box-w col-md-4">
+					<div class="box">
+						You get a peace of mind knowing that we will process and manage your claims professionally using our experiences, skill and resources.
+					</div>
+				</div>
+				<div class="box-w col-md-4">
+					<div class="box">
+						We are always abreast with the global insurance market and latest information innovations in providing enhanced Insurance Produts. 
+					</div>
+				</div>
+				<div class="box-w col-md-4">
+					<div class="box">
+						Our International Network with a string of Partners including <a href="http://www.wtwco.com" target="_blank">WTW</a>, <a href="http://www.global.lockton.com/au/en" target="_blank">Lockton</a>, <a href="http://absaviation.com.au" target="_blank">AUSTBROKERS</a>, <a href="https://www.ajg.com" target="_blank">Arthur J. Gallagher & Co.</a> and <a href="https://www.unisonsteadfast.com" target="_blank">unisonsteadfast</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section about-section" id="about">
+	<div class="slide">
+		<div class="about-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="lead">We are Insurance Brokers who can save you time, money and worry.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<p>Just like an accountant or lawyer who provides you with impartial professional advice based on years of training and experience. A qualified broker can do the same with your insurance. When arranging insurance, many people take shortcuts without seeking proper advice, understanding the fine print or considering whether they are getting value for money. Often they are disappointed when their insurance doesn't come to the rescue.</p>
+					</div>
+					<div class="col-md-4">
+						<p>Whether its home, car, life or business insurance, brokers provide advice and assistance to make sure you are properly protected. We have excess to varieties of policies because we deal with a range of insurance companies. Kanda International as a professional broker will be aware of the benefits, exclusions and costs of the competing policies on the market. We will also help arrange and place the cover and often provide advice on how to make.</p>
+					</div>
+					<div class="col-md-4">
+						<p>the most of your insurance budget. Using us would not necessarily cost more. Often it costs less because we have the knowledge of the insurance market and the ability to negotiated competitive premiums on your behalf. We will be obliged to advise you of fees charged for services provided for you.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h2>Our Goal</h2>
+					<p>Our goal is to create a long-term value for shareholders by producing superior sustained return.</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Our Vision</h2>
+					<p>Our vision is to be reputable Insurance Brokers, Risk Consultants and Risk Managers based in PNG, providing excellent services to our clients based on sound, professional and ethical principles.</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Our Scope</h2>
+					<p>The core of our business is to satisfy our customer needs for Insurance Products and Services, Risk Consultation and Risk Management</p>
+				</div>
+			</div>
+			<div class="row values">
+				<div class="col-md-8">
+					<h2>Our Values</h2>
+					<p>We strive to achieve our vision and goal by prompting the corporate values of:</p>
+					<ul>
+						<li>Financial Strength and Stability.</li>
+						<li>Integrity and Dedication.</li>
+						<li>Teamwork &amp; Effective Communication.</li>
+						<li>Innovators and Creativity.</li>
+						<li>Training and Personal Development.</li>
+						<li>Planning, Accountability and Rewards directly linked to results oriented performance.</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<h2>&nbsp;</h2>
+					<p>Promoting and effectively stewarding of these values will foster and environment of mutual trust, respect and partnership in which every employee will be encouraged to meaningfully contribute and discharge their duties and responsibilities to their full potential.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section service-section" id="service">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<br><br><br><p class="lead">We specialize in tailor made risk management solutions for our valuable customers insurance needs. kanda international provides  the following insurance products and services to meet changing market needs</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<h2>General Insurance</h2>
+				<p>For over 10-years, Kanda have been leaders in risk advice and general insurance and providing solutions for financial exposures for our local and overseas customers.</p>
+				<p><a href="/services.php">read more &rarr;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h2>Life Insurance</h2>
+				<p>At Kanda we understand that everyone has different life insurance needs based on their situation and stage in life. That's why our products are designed to be flexible so that they can be tailored to suit the ever changing needs.</p>
+				<p><a href="/services.php">read more &rarr;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h2>Claim Services</h2>
+				<p>At Kanda International Insurance brokers, we pride ourselves on the way we support our business and personal clients when they need to make a claim. we understand that even the smallest claim can be a worry and a hassle. That is why we are aways available for help. </p>
+				<p><a href="/services.php">read more &rarr;</a></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<h2>Risk Consulting</h2>
+				<p>Our Services will be available in the areas of Risk Surveys and Risk Improvement Advice. We can plan full Self Insurances Programs designed to meet your specific need.</p>
+				<p><a href="/services.php">read more &rarr;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h2>Premium Funding</h2>
+				<p>We have facilities available with Banks and Finance Institutions where premium can be obtained at very competitive rates. The procedure is very simple and is free of collateral. </p>
+				<p><a href="/services.php">read more &rarr;</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section staff-section" id="staff">
+	<div class="slide">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="profile-box">
+						<img src="images/staff/benn2.jpg" alt="">
+						<h3>Benn Piani Tiki <span>ANZIIF (Fellow) CIP, BCOM, MBA</span> <small>CEO &amp; Managing Director</small></h3>
+						<p>Mr. Tiki is accredited with the most prestigious Fellow award and a Certified Insurance Professional of the Australia and New Zealand Institute of Insurance and Finance with over twenty five (25) years' experience in the Insurance Industry. Mr. Tiki also holds a Masters In Business Administration (MBA) with the Divine Word University.</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="profile-box">
+						<img src="images/staff/roys.jpg" alt="">
+						<h3>Roy Salalila<small>Strategic development Manager</small></h3>
+						<p>He is the Strategic development Manager. Oliver has been in the insurance industry for more than 23 years in as an Underwriter, a Broker for global broking and underwriting firm in Philippines.He is a licensed Electrical Engineer, started his career in the industry as a Risk Engineer with main exposures to Power Plants and other Industrial Risks.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+				    <div class="profile-box">
+						<img src="images/staff/prasad.jpg" alt="">
+						<h3>Prasad Silva <span>Cert CII (UK)</span> <small>Client Manager, SME & CORPORATIONS</small></h3>
+						  <p>“Having over 18 years of experience in the insurance industry in Claims (Medical, Personal Accident & Workmen’s Compensation), Underwriting & Insurance broking, Prasad held multiple functions in top insurance broker companies in Sri Lanka. Prasad holds Certificate in Insurance (CertCII) in Chartered Insurance Institute in United Kingdom. Also, he is a member of Sri Lanka Insurance Institute.”</p>
+					</div>	
+				</div>
+				<div class="col-md-6">
+					<div class="profile-box">
+						<img src="images/staff/norman.jpg" alt="">
+						<h3>Norman <span>ANZIIF (Associate) CIP</span> <small>MANAGER, CORPORATE CLIENT</small></h3>
+						  <p>Norman is the Manager, Corporate Clients. He is qualified as a Senior Associate of the Australian and New Zealand Insurance Institute. He has over 10 years’ extensive experience in facets of insurance covering a wide range of industries. Risk specialty includes Commercial Risks and Employee Benefits.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="profile-box">
+						<img src="images/staff/tom.jpg" alt="">
+						<h3>Tom Kune <small>Lae Branch Manager</small></h3>
+						  <p>He is our Lae Office Manager. He has over thirty (30) years experience in the Insurance Industry. He has been a Senior Insurance Broker with International Insurance companies he worked for.</p>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section contact-section" id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p>Thank you for visiting our web site. We at Kanda international are committed to provide you of our best service every time. Your feedback and suggestions about our products and services are more important for us to serve you even better.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<b>HEAD OFFICE - PORT MORESBY</b><br>
+				Address: Level2, Century 21 House, Kunai Street, Off Waigaini Drive<br>
+				Postal Address: P O Box 1330, Port Moresby,NCD<br>
+				TELEPHONE: (675) 323 9572 / 323 9643 /323 9849<br>
+				MOBILE: (675) 7686 3562 / 7686 9453<br>
+				FAX: (675) 323 9535<br>
+				EMAIL: btiki@kanda.com.pg
+			</div>
+			<div class="col-md-8">
+				<b>LAE OFFICE</b><br>
+				Address: Suites 2.10 & 2.70, 2nd Floor, JT's, Maman Anda Haus<br> 
+                Cnr 4th Street, Huon Road, Top Town, LAE<br>
+				Postal Address: P.O Box 2328, Lae 411, Morobe Province<br> 
+				TELEPHONE: (675) 472 8474<br>
+				FAX: (675) 472 8547<br>
+				EMAIL: tkune@kanda.com.pg
+			</div>
+		</div>
+	</div>
+</section>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/vendors/jquery.easings.min.js"></script> 
+<script src="js/vendors/jquery.slimscroll.min.js"></script>
+<script src="js/jquery.fullPage.min.js"></script>
+<script src="js/skrollr.min.js"></script>
+<script type="text/javascript">
+$(function(){
+	var s = skrollr.init();
+	if($(document).width()>480){
+		$('#intro').addClass('down');
+		 $('#kanda').fullpage({
+		 	slidesColor: ['#1175b9', '#1175b9', '#7BAABE', '#1175b9', '#ccddff', '#1bbc9b'],
+			anchors: ['home', 'whykanda', 'aboutus', 'services', 'ourstaff', 'contactus'],
+			menu: '#menu',
+			afterLoad: function(anchorLink, index){
+				if(index == '1'){
+	                $('.navbar-brand').removeClass('active');
+	            }
+
+	            if(index > '1'){
+	                $('.navbar-brand').addClass('active');
+	            }
+			},
+			afterRender: function(){
+				$('#kanda-home').addClass('slide');
+	            $('#loading').fadeOut(function(){
+	            	$('#intro').removeClass('down');
+	            });
+	        }
+		 });
+	 }
+});
+</script>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html class="inner-html">
+<head>
+<meta charset="UTF-8">
+<title>Kanda International</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/main.css">
+<!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body class="inner-page">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#kanda-nav">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	      	</button>
+	     	<a class="navbar-brand" href="/">
+	     		<img src="images/logo-img.png" alt="">
+	     	</a>
+	    </div>
+		<div class="collapse navbar-collapse" id="kanda-nav">
+			<ul class="nav navbar-nav navbar-right" id="menu">
+				<li data-menuanchor="home" class=""><a href="/#home"><span>Home</span></a></li>
+				<li data-menuanchor="whykanda" class=""><a href="/#whykanda"><span>Why Kanda</span></a></li>
+				<li data-menuanchor="aboutus" class=""><a href="/#aboutus"><span>About Us</span></a></li>
+				<li data-menuanchor="services"><a href="/#services"><span>Services</span></a></li>
+				<li data-menuanchor="ourstaff" class=""><a href="/#ourstaff"><span>Our Staff</span></a></li>
+				<li data-menuanchor="contactus"><a href="/#contactus"><span>Contact Us</span></a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div id="kanda-carousel" class="carousel kanda-carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#kanda-carousel" data-slide-to="0" class="active"></li>
+		<li data-target="#kanda-carousel" data-slide-to="1"></li>
+		<li data-target="#kanda-carousel" data-slide-to="2"></li>
+		<li data-target="#kanda-carousel" data-slide-to="3"></li>
+		<li data-target="#kanda-carousel" data-slide-to="4"></li>
+		<li data-target="#kanda-carousel" data-slide-to="5"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item active" style="background-image: url(images/1.jpeg);"></div>
+		<div class="item" style="background-image: url(images/3.jpg);"></div>
+		<div class="item" style="background-image: url(images/4.jpg);"></div>
+		<div class="item" style="background-image: url(images/5.jpg);"></div>
+		<div class="item" style="background-image: url(images/6.jpg);"></div>
+		<div class="item" style="background-image: url(images/8.jpg);"></div>
+
+	</div>
+</div>
+<div class="container">
+	<div class="row insurance-list">
+		<div class="col-md-4">
+			<h2>General Insurance</h2>
+			<dl>
+				<dt>Motor -</dt>
+				<dd>Private</dd>
+				<dd>Commercial fleets</dd>
+				<dt>Fire - </dt>
+				<dd>industrial special risk</dd>
+				<dd>Fire  commercial</dd>
+				<dd>Private dwelling</dd>
+				<dd>Business interruption</dd>
+				<dt>Engineering -</dt>
+				<dd>Contractors special risk</dd>
+				<dd>Electronic equipment</dd>
+				<dd>Boiler  pressure vessel</dd>
+				<dd>Erection of Machinery</dd>
+				<dd>Machinery all risk</dd>
+				<dt>Aviation</dt>
+				<dd>Hull insurance</dd>
+				<dd>Passenger Liability</dd>
+				<dd>Aviation Liability </dd>
+			</dl>
+		</div>
+		<div class="col-md-4">
+			<h2>&nbsp;</h2>
+			<dl>
+				<dt>Miscellaneous Insurance</dt>
+				<dd>Money</dd>
+				<dd>Burglary</dd>
+				<dd>Workmen's compensation</dd>
+				<dd>Personal accident</dd>
+				<dd>Travel insurance</dd>
+				<dd>Fidelity grantee</dd>
+				<dd>Directors &amp; Officers Liability </dd>
+				<dd>Public  Product liability</dd>
+				<dd>Professional indemnity</dd>
+				<dd>Medical and Emergency Evaluations</dd>
+				<dt>Marine</dt>
+				<dd>Marine Cargo</dd>
+				<dd>Hull insurance</dd>
+				<dd>Pleasure craft</dd>
+				<dd>Protection &amp; Indemnity</dd>
+			</dl>
+		</div>
+		<div class="col-md-4">
+			<h2>Life Insurance</h2>
+			<dl>
+				<dd>Group Tem Life</dd>
+				<dd>Trauma/Critical illness</dd>
+				<dd>Loss of income</dd>
+			</dl>
+		</div>
+	</div>
+	<h2>Claim services</h2>
+	<div class="page-content">
+		<p>At Kanda international insurance brokers, we pride ourselves on the way we support our business and personal clients when they need to make a claim. we understand that even the smallest claim can be a worry and a hassle. That is why we are aways available for help. </p>
+		<p>We have specially trained staff who will assist in your claims Management. We will have a fast turnaround on the claims submitted to us. Kanda international recognizes that our Client's insurance are realized when the insured event catastrophe occurs. Therefore, we are resourced to give you the maximum assistant possible at the time when you need us the most, claims time. Large claims can be managed in liaison with the appointed Loss Adjusters who are professionals in this area. Kanda International will also liaise closely with the Lawyers in resolving your claims at the most diligent and efficient manner. Remember that Kanda International will always act on behalf of the insured (Our Clients) not the insurers.</p>
+		<p><b>It is obvious that in the event of an incident which will or may give rise to a claim, a procedure must be adopted for the proper and expeditious conduct of the claim. By carefully following this procedure, the impact on your business operations will be minimized.</b></p>
+		<p>There are number of steps which must be taken following a loss:</p>
+		<p>Report the incident to the Kanda International office that looks after your insurance program, by telephone, facsimile or email - wherever practical, within 24 hours of the incident.</p>
+		<p>Regardless of whether or not the claim has been reported or a loss assessor appointed, you must immediately do whatever is necessary to prevent further loss of life or property damage.</p>
+		<ul>
+			<li>Whatever the circumstances of the incident, <b>DO NOT ADMIT LIABILITY</b> EVEN IF YOU THINK YOU ARE AT FAULT.</li>
+			<li>Your insurer is entitled to deny a claim or pay a reduced amount if statements made by you or your employees prejudice the insurer's position.</li>
+			<li>Report the Incident to your nearest police station and provide a statement.</li>
+		</ul>
+		<p>It is impossible to give guidelines for procedures in every claim, simply because of the nature of accidents:</p>
+		<ul>
+			<li>They cannot be predicted</li>
+			<li>They do not follow set patterns</li>
+		</ul>
+		<p>However, the general procedures for various major classes of insurance detailed above, can be relied upon to cover most circumstances. For any other claim involving other classes of insurance, please always contact your Kanda Insurance Brokers office without delay.</p>
+	</div>
+	<h2>Risk Consulting</h2>
+	<div class="page-content">
+		<p>Our Services will be available in the areas of Risk Surveys and Risk Improvement Advice. We can plan full Self Insurances Programs designed to meet your specific need.</p>
+		<p>At Kanda International we recognize that Our Clients do not have to , Transfer Risks to Underwriters by buying Insurance Policies. They can save premiums by way of Risk Improvement Programs and Self Insurance Strategies. Our clients will be recommended to take out insurances on identified risks that can be too big to carry and are detrimental to their operation in the even these catastrophes occur.</p>
+	</div>
+	<h2>Premium Funding</h2>
+	<div class="page-content">
+		<p>We have facilities available with Banks and Finance Institutions where premium can be obtained at very competitive rates. The procedure is very simple and is free of collateral. </p>
+		<p>Payment of insurance premiums annually in advance can place a financial strain on any business. In the current economic climate it is imperative that you optimise credit terms, allowing expense to be managed in the most efficient manner. </p>
+		<p>Spreading the cost of insurance premiums through a structured monthly repayment program produces the following benefits:</p>
+		<ul>
+			<li>Preserves existing lines of credit for traditional working capital requirements</li>
+			<li>Assists cash flow management</li>
+			<li>Premiums are a business expense, thus interest costs are tax deductible</li>
+			<li>Creates a separate line of credit</li>
+			<li>Premium financiers require no physical security. Their security takes the form of a lien over the unexpired portion of your insurance premiums.</li>
+		</ul>
+		<p>Please advise our office (<a href="mailto:">Benn</a> or <a href="mailto:">Shamal</a> if you require a quotation to fund your premiums.</p>
+	</div>
+</div>
+<footer class="footer text-center">
+	<ul class="list-unstyled list-inline">
+		<li><a href="/#home"><span>Home</span></a></li>
+		<li><a href="/#whykanda"><span>Why Kanda</span></a></li>
+		<li><a href="/#aboutus"><span>About Us</span></a></li>
+		<li><a href="/#services"><span>Services</span></a></li>
+		<li><a href="/#ourstaff"><span>Our Staff</span></a></li>
+		<li><a href="/#contactus"><span>Contact Us</span></a></li>
+	</ul>
+</footer>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
